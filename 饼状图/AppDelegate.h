5e2fe91ac88d3465e82dd3ff93ea2c6440eca0dd
残月 <632342527@qq.com>
2016-07-10 10:25:04 +0800

    Initial Commit

---
+++ 饼状图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  饼状图
+//
+//  Created by 王奥东 on 16/7/10.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 饼状图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  饼状图
+//
+//  Created by 王奥东 on 16/7/10.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
